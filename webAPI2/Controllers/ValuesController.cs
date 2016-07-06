f308fa533c645321901025586c32f88b402db932
Bo Chen <bochen2014@yahoo.com>
2016-07-06 23:51:40 +1000

    update

--- webAPI2/Controllers/ValuesController.cs
+++ webAPI2/Controllers/ValuesController.cs
@@ -22,12 +22,14 @@ namespace webAPI2.Controllers
             return "value";
         }
 
-        // POST api/values
+        // POST(CREATE) api/values
+        /*a value may be passed in the iurl or in the body. If a value is passed in both, the url value is used, 
+         * the [FromBody] say to not use the url but only the body*/
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT(UPDATE) api/values/5
         public void Put(int id, [FromBody]string value)
         {
         }
