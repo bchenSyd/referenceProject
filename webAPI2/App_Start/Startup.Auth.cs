846420332e563685072a169b8a0e4c477de3500b
Bo Chen <bochen2014@yahoo.com>
2016-08-11 20:27:11 +1000

    update from visual studio

--- webAPI2/App_Start/Startup.Auth.cs
+++ webAPI2/App_Start/Startup.Auth.cs
@@ -39,7 +39,7 @@ namespace webAPI2
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context and user manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(ApplicationDbContext.Create); //this is the sandard way of managing dbcontext
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
